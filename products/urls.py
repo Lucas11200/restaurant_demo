cff69a20a110331c4e0e7229a08005b4b706c593
lucas11200 <lucas11200@hotmail.com>
2023-01-04 14:44:48 +0000

    chore(urls): remove trailing slash from path

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,6 @@ from restaurant_demo.settings import GET_PRODUCTS_API_NAME
 allowed_methods = {"get": "get", "post": "post", "delete": "delete", "patch": "update"}
 
 urlpatterns = [
-    path('/', ProductsView.as_view(allowed_methods), name=GET_PRODUCTS_API_NAME),
+    path('', ProductsView.as_view(allowed_methods), name=GET_PRODUCTS_API_NAME),
     # path('/customizations', CustomizationsView.as_view(allowed_methods)),
 ]
